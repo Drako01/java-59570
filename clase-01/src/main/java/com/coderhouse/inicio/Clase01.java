45f22815ec1f5416f0998b5f952e874cbe5c4d83
Alejandro Di Stefano <addistefano@live.com.ar>
2024-09-26 20:59:23 -0300

    Clase 01

---
+++ clase-01/src/main/java/com/coderhouse/inicio/Clase01.java
@@ -0,0 +1,57 @@
+package com.coderhouse.inicio;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// Declaracion de variables
+		
+		/*int variableA = 1000;
+		
+		byte variableB = 12;
+		
+		/*var numero = 1;
+		numero = 10;*/
+		
+		/*String numeroA = "Hola";
+		
+		char letra = '@';
+		String letraA = "b";
+		
+		String letraB = "dfsfsd";
+		
+		//System.out.println("El resultado es: " + (variableA + variableB));
+		
+		System.out.println(variableA);*/
+		/*
+		float numero1 = 10f;
+		float numero2 = 12f;
+		float resultado;
+		
+		// Calculadora
+		
+		resultado = numero1 / numero2;
+		
+		System.out.println("El resultado es: " + resultado);
+		*/
+		
+		// Incrementos
+		int contador = 1;
+		
+		System.out.println(contador);
+		contador = contador + 1;
+		System.out.println(contador);
+		contador = contador + 1;
+		System.out.println(contador);
+		contador--;
+		System.out.println(contador);
+		contador--;
+		System.out.println(contador);
+		
+		// contador == contador; Con esto comparo el valor de dos variables
+		// contador === contador; Con esto comparo el valor y el tipo de dos variables
+		
+		
+		
+	}
+
+}
